ba1c0686a0b8bd7b3164d867d949b26e9a2bda02
Barbara Barbabela <barbarabarbabela@gmail.com>
2023-07-15 17:46:33 -0300

    Criando a seção de Análise Geral do projeto

---
+++ desafio-robos/src/components/AnaliseGeral/index.js
@@ -0,0 +1,13 @@
+import './AnaliseGeral.css'
+import React  from 'react';
+
+//Criando o cabeçalho do projeto
+const AnaliseGeral = () => {
+  return <div className="secao_principal">
+    <img src="/assets/analise-logo.png" alt="Logo da sessão inicial"></img>
+    <div className="geral_separador"></div>
+    <h1>Análise Geral <span className="titulo_estilo">/</span> <span className="titulo_principal">Principal</span></h1>
+  </div>
+}
+
+export default AnaliseGeral
